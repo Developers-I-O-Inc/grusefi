636bec36b25ab0143c6a7c232b648e21c681ac26
Manuel de Jesus Morales Sanchez <bechy10@live.com.mx>
2024-07-01 09:15:20 -0600

    Prepare template grusefi

--- routes/operation.php
+++ routes/operation.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 // DICTAMEN DE VERIFICACION
 Route::get('dictamen_verificacion', 'DictamenVerificacionController@dictamen_verificacion');
+// PLANTILLAS
+Route::get('plantillas_rpv', 'PlantillasController@plantillas_rpv');
