98f6b5fcf6e21bb02045dc1ebeb613b626300629
unknown <bechy10@live.com.mx>
2024-11-13 14:40:17 -0600

    refactor: Add field variedad and save in embarques

--- database/migrations/2024_09_30_235702_create_productos_embarques_table.php
+++ database/migrations/2024_09_30_235702_create_productos_embarques_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('embarque_id')->unsigned()->index();
             $table->bigInteger('categoria_id')->unsigned()->index();
-            $table->bigInteger('tipo_cultivo_id')->unsigned()->index();
             $table->bigInteger('presentacion_id')->unsigned()->index();
             $table->bigInteger('calibre_id')->unsigned()->index();
             $table->string('folio_pallet');
@@ -26,7 +25,6 @@ return new class extends Migration
             $table->string('n_registros')->default(1);
             $table->foreign('embarque_id')->references('id')->on('op_embarques');
             $table->foreign('categoria_id')->references('id')->on('cat_categorias');
-            $table->foreign('tipo_cultivo_id')->references('id')->on('cat_tipo_cultivos');
             $table->foreign('presentacion_id')->references('id')->on('cat_presentaciones');
             $table->foreign('calibre_id')->references('id')->on('cat_calibres');
             $table->timestamps();
